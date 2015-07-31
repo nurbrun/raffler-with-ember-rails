0e79d5ed5be4b1c4e433e5639fbd1523b0fff720
Bruno Alfano-Rizzo <bruno.alfano.rizzo@gmail.com>
2015-07-31 10:55:35 -0400

    added ember-rails to gem file, ran bundler and generated ember.js files. configured application.js file to require necessary files

--- raffler/app/assets/javascripts/application.js
+++ raffler/app/assets/javascripts/application.js
@@ -13,4 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require ember
+//= require ember-data
+//= require active-model-adapter
+//= require_self
+//= require raffler
+
+Raffler = Ember.Application.create()
